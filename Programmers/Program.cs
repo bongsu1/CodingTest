a00485936d02e4081c033ec54aea50ac339c7a8c
bongsu <rlaqhdtn22@gmail.com>
2024-02-08 09:52:27 +0900

    docs : RicochetRobot(리코쳇 로봇)
    풀이 미완료

--- Programmers/Program.cs
+++ Programmers/Program.cs
@@ -4,9 +4,9 @@
     {
         static void Main(string[] args)
         {
-            int[,] wires = { { 1, 3 }, { 2, 3 }, { 3, 4 }, { 4, 5 }, { 4, 6 }, { 4, 7 }, { 7, 8 }, { 7, 9 } };
-            int split = SplittingThePowerGridInTwo.Solution(9, wires);
-            Console.WriteLine(split);
+            string[] board = { "...D..R", ".D.G...", "....D.D", "D....D.", "..D...." };
+            int shortest = RicochetRobot.Solution(board);
+            Console.WriteLine(shortest);
         }
     }
 }
---
+++ Programmers/RicochetRobot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers
+{
+    internal class RicochetRobot
+    {
+        public static int Solution(string[] board)
+        {
+            int xSize = board[0].Length;
+            int ySize = board.Length;
+            int answer = -1;
+            char[,] graph = new char[ySize, xSize];
+            for (int i = 0; i < ySize; i++)
+            {
+                for (int j = 0; j < xSize; j++)
+                {
+                    graph[i, j] = board[i][j];
+                    if (board[i][j] == 'R')
+
+                }
+            }
+
+
+            return answer;
+        }
+
+        
+    }
+}
